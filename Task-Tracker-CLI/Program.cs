ac0632f9b2c50dfe012bf33d94aba692781761a9
James T <jamesht512@gmail.com>
2025-07-18 15:56:14 -0700

    Add basic CLI interaction

--- Task-Tracker-CLI/Program.cs
+++ Task-Tracker-CLI/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Hello and welcome to the Task Tracker App!");
+
+bool active = true;
+while (active)
+{
+    Console.WriteLine("--------------------------");
+    Console.WriteLine("What would you like to do?");
+    string input = Console.ReadLine() ?? string.Empty;
+
+    switch (input) 
+    {
+        case "":
+            Console.WriteLine("You entered an empty command. Please try again.");
+            break;
+
+        case "done":
+            Console.WriteLine("Exiting the Task Tracker App. Goodbye!");
+            active = false;
+            break;
+    }
+}
